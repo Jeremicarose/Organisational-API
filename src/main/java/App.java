df8576de901cb83b144527b16fab86ed1616e035
Jeremic Arose <arosejeremic5@gmail.com>
2019-08-13 20:19:07 +0300

    made changes on post

---
+++ src/main/java/App.java
@@ -0,0 +1,76 @@
+
+import DB.DB;
+import com.google.gson.Gson;
+import dao.Sql2oDepartmentDao;
+import dao.Sql2oNewsDao;
+import dao.Sql2oUserDao;
+import models.Department;
+import models.News;
+import models.User;
+
+import static spark.Spark.*;
+
+public class App {
+
+
+    public static void main(String[] args) {
+
+        staticFileLocation("/public");
+
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        }else {
+            port = 4567;
+        }
+        port(port);
+
+        Sql2oUserDao userDao = new Sql2oUserDao(DB.sql2o);
+        Sql2oNewsDao newsDao = new Sql2oNewsDao(DB.sql2o);
+        Sql2oDepartmentDao departmentDao = new Sql2oDepartmentDao(DB.sql2o);
+        final String notAvailableMsg = "Sorry, we do not have any %s to display currently",notAvailable;
+        final String cannotBeEmptyMsg = "Warning!!!, %s cannot be empty!!!, Please try again",cannotBeEmpty;
+        Gson gson = new Gson();
+
+
+        get("/users", "application/json",(req, res) -> {
+            res.type("application/json");
+            return gson.toJson(userDao.getAll());
+        });
+        get("/news", "application/json",(req, res) ->{
+            res.type("application/json");
+            return gson.toJson(newsDao.getAll());
+        });
+        get("/departments", "application/json",(req, res) ->{
+            res.type("application/json");
+            return gson.toJson(departmentDao.getAll());
+        });
+
+        post("/users/new", "application/json", (req, res)->{
+            User user = gson.fromJson(req.body(), User.class);
+            userDao.add(user);
+            res.type("application/json");
+            res.status(201);
+            return gson.toJson(user);
+        });
+        post("/news/new", "application/json", (req, res)->{
+            News news = gson.fromJson(req.body(), News.class);
+            newsDao.add(news);
+            res.type("application/json");
+            res.status(201);
+            return gson.toJson(news);
+        });
+        post("/department/new", "application/json", (req, res) -> {
+            Department department = gson.fromJson(req.body(), Department.class);
+            departmentDao.add(department);
+            res.type("application/json");
+            res.status(201);
+            return gson.toJson(department);
+        });
+
+
+
+    }
+}
--- src/test/java/DBRule/DbRule.java
+++ src/test/java/DBRule/DbRule.java
@@ -2,8 +2,6 @@ package DBRule;
 
 import dao.Sql2oDepartmentDao;
 import dao.Sql2oUserDao;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.rules.ExternalResource;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
@@ -16,7 +14,7 @@ public class DbRule extends ExternalResource {
 
     @Override
     public void before(){
-        String connectionString = "jdbc:postgresql://localhost:5432/org_api_test";
+        String connectionString = "jdbc:postgresql://localhost:5432/org_api";
         Sql2o sql2o = new Sql2o(connectionString,"moringa","1234" );
         userDao = new Sql2oUserDao(sql2o);
         departmentDao = new Sql2oDepartmentDao(sql2o);
