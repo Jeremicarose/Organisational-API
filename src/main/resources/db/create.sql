2c12b84f1332a1633689b27c96dd85efb67d5b3d
Jeremic Arose <arosejeremic5@gmail.com>
2019-08-13 14:38:17 +0300

    updated app.java

---
+++ src/main/resources/db/create.sql
@@ -0,0 +1,14 @@
+SET MODE postgreSQL;
+
+CREATE TABLE IF NOT EXISTS departments(
+
+);
+CREATE TABLE IF NOT EXISTS news(
+
+);
+CREATE TABLE IF NOT EXISTS user(
+
+);
+CREATE TABLE IF NOT EXISTS departments(
+
+    );
