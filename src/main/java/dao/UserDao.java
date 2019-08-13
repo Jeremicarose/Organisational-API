296c853bd16a61de429851130050fb3e504c3122
Jeremic Arose <arosejeremic5@gmail.com>
2019-08-13 11:47:14 +0300

    update inteface

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,4 +1,16 @@
 package dao;
 
+import models.User;
+
+import java.util.List;
+
 public interface UserDao {
+
+    void add(User user);
+    List<User> getAll();
+    List<User> getAllUsersByDepartment(int departmentId);
+    User findById(int id);
+    void deleteById(int id);
+    void clearAll();
 }
+
