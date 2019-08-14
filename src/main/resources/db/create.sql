d947fe694a8b7d3555596cc0b58943fc5df9e156
Jeremic Arose <arosejeremic5@gmail.com>
2019-08-14 11:35:50 +0300

    add sqlfile

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -1,14 +1,33 @@
 SET MODE postgreSQL;
 
 CREATE TABLE IF NOT EXISTS departments(
+id int PRIMARY KEY auto_increment,
+     departmentName VARCHAR;
+     description VARCHAR;
+     totalEmployees int;
+     id int;
 
 );
+
+
+
 CREATE TABLE IF NOT EXISTS news(
-
+id int PRIMARY KEY auto_increment,
+    news VARCHAR,
+    departmentId int,
+    id int,
 );
+
+
+
 CREATE TABLE IF NOT EXISTS user(
-
+id int PRIMARY KEY auto_increment,
+    username VARCHAR,
+    address VARCHAR,
+    phone VARCHAR,
+    email VARCHAR,
+    departmentId int,
+    position VARCHAR,
+    roles VARCHAR,
 );
-CREATE TABLE IF NOT EXISTS departments(
 
-    );
